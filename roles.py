0b01c3219bac660d336ae818bfbee596b3319270
moudii04 <91603056+moudii04@users.noreply.github.com>
2021-10-11 22:29:46 +0100

    Add files via upload

---
+++ roles.py
@@ -0,0 +1,6 @@
+champs = ["Tank", "Mage", "Assassin ", "Support"]
+champs_def = ["Un guerrier qui n'a pas peur de foncer dans le tas",
+              "Un sorcier qui fait appelle a la magie",
+              "Un expert en furtivité et maitre des dagues",
+              "Un puissant mage qui dédie ses pouvoirs a aider ses alliés"
+              ]
